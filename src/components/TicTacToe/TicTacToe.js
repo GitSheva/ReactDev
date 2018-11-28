d1336251f5d8658017bb629baf0b78a2279b7ff8
GitSheva <aryabko@gmail.com>
2018-11-28 15:37:23 -0800

    upd

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -3,17 +3,30 @@ import './TicTacToe.css';
 import Square from '../Square/Square.js';
 
 class TicTacToe extends Component {
+    constructor (props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+    
     render() {
         return (
         <div>
-            <div>
-                <Square/><Square/><Square/>
+            <div className="board-row">
+                <Square value={this.state.squares[0]} />
+                <Square value={this.state.squares[1]} />
+                <Square value={this.state.squares[2]}/>
             </div> 
-            <div>
-                <Square/><Square/><Square/>
+            <div className="board-row">
+                <Square value={this.state.squares[3]} />
+                <Square value={this.state.squares[4]} />
+                <Square value={this.state.squares[5]} />
             </div> 
-            <div>
-                <Square/><Square/><Square/>
+            <div className="board-row">
+                <Square value={this.state.squares[6]} />
+                <Square value={this.state.squares[7]} />
+                <Square value={this.state.squares[8]} />
             </div>
         </div>
         );
